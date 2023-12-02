ea7a59fcbd8f0f7815658b334845f07b7be95caf
Jonhlocke <141368958+Jonhlocke@users.noreply.github.com>
2023-12-01 23:01:32 -0300

    Update mapa.h

--- mapa.h
+++ mapa.h
@@ -8,6 +8,7 @@
 //media de 12 inimigos antes do boss/ 0=descanso, 1=loja, 2 até 21=inimigos(20 possiveis inimigos), 30=boss
 //boa sorte
 
+
 typedef int tp_item; //o nodo armazena um numero que identifica se é um descanso, loja ou inimigo, e ser for inimigo, qual
 
 
@@ -33,15 +34,16 @@ tp_nodo* cria_nodo(tp_nodo *atu)
     novo_no->cam1=NULL;
     //printf("teste\n");
 
-    if(atu->contno==19)//esse numero mais 1 é a quantidade de nodos que serao criados ate ser criado o nodo que sera um boss, e portanto o fim do jogo
+    if(atu->contno==9)//esse numero mais 1 é a quantidade de nodos que serao criados ate ser criado o nodo que sera um boss, e portanto o fim do jogo
     {
-        novo_no->fase=30;
+        novo_no->fase=11;//30 antes
         novo_no->cam1=novo_no;
         novo_no->cam2=novo_no;
         return novo_no;
     }
     
-    if((rand() % 4) == 0)// isso da 25% de chance de haver um caminho alternativo nesse no
+    
+    if((rand() % 4) != 0)// isso da 75% de chance de haver um caminho alternativo nesse no
     {
         novo_no->cam2=NULL;
     }
@@ -50,16 +52,28 @@ tp_nodo* cria_nodo(tp_nodo *atu)
         novo_no->cam2=novo_no;//significa que não ha caminho alternativo, ele aponta pra si mesmo
     }
     
-    
-    if((rand() % 5) == 0) //isso da uma chance de 20% de o nodo ser um descanso ou loja
+    static int contD=0, contL=0;//contador de vezes que aparece loja e descanso
+    if((rand() % 5 + contD + contL) == 2) //isso da uma chance de 20% de o nodo ser um descanso ou loja
+    //impede que tenha mais de 3 lojas e descansos 
     {
         
         novo_no->fase=rand() % 2;//há 50% de chance de retornar 0, tornando o nodo um descanso, e 50% de retornar 1 tornando-o uma loja
+        if (novo_no->fase == 0) {
+        if (contD < 2) {
+            contD++;
+            return novo_no;
+        }
+    } else {
+        if (contL < 2) {
+            contL++;
+            return novo_no;
+        }
+
         return novo_no;
     }
-    else
-    {
-        novo_no->fase=(rand() % 20) + 2;//o 20 é a quantidade de inimigos possiveis a serem escolhidos, o 2 é para evitar conflito com loja ou descanso
+    } else {
+        //mudou
+        novo_no->fase=(rand() % 8) + 2;//o 20 é a quantidade de inimigos possiveis a serem escolhidos, o 2 é para evitar conflito com loja ou descanso
         return novo_no;
     }
 }
@@ -95,6 +109,34 @@ void montagem_geral(tp_nodo *atu)//isso é uma função recursiva, ela monta a a
     //printf("final alcancado\n");
 }
 
+void verificaFase(int fase){
+    if(fase>1 && fase<10) printf("Inimigo\n");
+        else if(fase==0) printf("Descanso\n");
+        else if(fase==1) printf("Loja\n");
+        else if(fase==11) printf("Boss\n");
+}
+
+void registra(FILE **arquivo, int jogo, char *nome_arquivo) {//regista em arquivo txt o nivel que o usuario alcancou
+    int tentativas = 0;
+    while (tentativas < 100) { // Limita as tentativas para evitar loops infinitos
+        snprintf(nome_arquivo, 20, "jogo_%d_nivel.txt", jogo);//nome do arquivo é jogo_numero_nivel
+        *arquivo = fopen(nome_arquivo, "r");
+        
+        if (*arquivo == NULL) {
+            *arquivo = fopen(nome_arquivo, "w");
+            if (*arquivo == NULL) {
+                printf("Erro ao abrir o arquivo.");
+                return;
+            }
+            return;
+        } else {
+            fclose(*arquivo);
+            jogo++;
+        }
+        tentativas++;
+    }
+    printf("Limite de tentativas excedido. Verifique os arquivos existentes.");
+}//toda vez que o codigo for rodado sera salvo referente ao seu numero, sor for a primeira vez que foi rodado sera salvo como jogo_1_nivel
 
 void prepara_mapa(tp_nodo *mapa)
 {
@@ -132,7 +174,7 @@ tp_item navega_mapa(tp_nodo *mapa)
     case 1:
     printf("loja, e o outro para um ");
         break;
-    case 30:
+    case 11:
     printf("Boss, e o outro para um ");
         break;
     default:
@@ -148,7 +190,7 @@ tp_item navega_mapa(tp_nodo *mapa)
     case 1:
     printf("loja. Voce toma o caminho 1 ou 2?\n");
         break;
-    case 30:
+    case 11:
     printf("Boss. Voce toma o caminho 1 ou 2?\n");
         break;
     default:
@@ -167,15 +209,6 @@ tp_item navega_mapa(tp_nodo *mapa)
             mapa->cam2=mapa->cam2->cam2;
             return mapa->cam2->fase;
         }
-  }
+    }
 }
-
-void verificaFase(int fase){
-    if(fase>1 && fase<30) printf("Inimigo\n");
-        if(fase==0) printf("Descanso\n");
-        if(fase==1) printf("Loja\n");
-        if(fase==30) printf("Boss\n");
-}
-
-
 #endif
